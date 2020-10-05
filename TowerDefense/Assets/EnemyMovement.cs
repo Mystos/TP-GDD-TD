1437f192110f9a487525e42725d4b57ce21fd65c
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-05 19:34:49 +0200

    Ajout projet TD

---
+++ TowerDefense/Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public LinkedList<GameObject> waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(!transform.position.Equals(waypoints.First.Value.transform.position))
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints.First.Value.transform.position, Time.deltaTime);
+        }
+        else
+        {
+            waypoints.RemoveFirst();
+        }
+    }
+}
---
+++ TowerDefense/Assets/Scripts/GridTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridTest : MonoBehaviour
+{
+    public int x, y;
+    public Transform player;
+    public Tilemap tilemap;
+    public Vector3 originPosition;
+    Grid grid;
+    void Start()
+    {
+        grid = new Grid(x, y, 1f, originPosition);
+        Debug.Log(tilemap.size);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            grid.GetXY(vec, out int x, out int y);
+            Debug.Log(x + ":" + y);
+
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        vec.z = 0f;
+        return vec;
+    }
+
+}
